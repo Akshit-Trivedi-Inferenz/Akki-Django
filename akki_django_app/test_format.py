97798040688912dad6506267da8c36a223d41f78
Akshit-Trivedi-Inferenz <146056545+Akshit-Trivedi-Inferenz@users.noreply.github.com>
2023-12-22 12:53:04 +0530

    Update test_format.py

--- akki_django_app/test_format.py
+++ akki_django_app/test_format.py
@@ -10,15 +10,15 @@ print("This function is missing a colon")
 z = 4
 if z > 2:
 print("z is greater than 2")
-else
+else:
 print("z is not greater than 2")
 return z
 
 def main():
 print("This is the main function")
-for i in range(5)
+for i in range(5):
 print(f"Value: {i}")
 print("End of main function")
 
-if __name__ == "__main__"
+if __name__ == "__main__":
 main()
