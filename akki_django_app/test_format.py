666af349d248857b80aafe74033c45f46d342454
Akshit-Trivedi-Inferenz <146056545+Akshit-Trivedi-Inferenz@users.noreply.github.com>
2023-12-22 12:33:34 +0530

    Update test_format.py

--- akki_django_app/test_format.py
+++ akki_django_app/test_format.py
@@ -1,20 +1,20 @@
 import math, random
 
-def poorly_formatted_function(x)
+def poorly_formatted_function(x):
 print("This function is missing proper indentation")
 y = 2 * x + 3
 return y
 
-def another_poorly_formatted_function()
+def another_poorly_formatted_function():
 print("This function is missing a colon")
 z = 4
-if z > 2
+if z > 2:
 print("z is greater than 2")
 else
 print("z is not greater than 2")
 return z
 
-def main()
+def main():
 print("This is the main function")
 for i in range(5)
 print(f"Value: {i}")
